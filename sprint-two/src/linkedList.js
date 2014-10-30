50d2c742627cc14007a3d9c34063119d5b049921
grantbot <grantwu2@gmail.com>
2014-10-30 10:54:54 -0700

    Implemented linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = makeNode(value);
+    if (list.head == null) {
+      list.tail = node;
+      list.head = list.tail;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function(){
+    var temp = list.head;
+    delete list.head;
+    list.head = temp.next;
+    return temp.value;
   };
 
   list.contains = function(target){
+    var recurse = function(node) {
+      if (node.value == target){
+        return true;
+      } else if (node.next == null) {
+        return false;
+      } else {
+        return recurse(node.next);
+      }
+    };
+
+    return recurse(list.head);
   };
 
   return list;
