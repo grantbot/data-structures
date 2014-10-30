da3d87b77332d1a831ae00661ca84e47cc292b62
grantbot <grantwu2@gmail.com>
2014-10-30 11:13:51 -0700

    Implemented Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,31 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = '';
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if (this._storage.indexOf(item) === -1) {
+    this._storage += item;
+  }
 };
 
 setPrototype.contains = function(item){
+  if (this._storage.indexOf(item) !== -1) {
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item){
+  if (this._storage.indexOf(item) !== -1) {
+    var index = this._storage.indexOf(item);
+    this._storage = this._storage.substring(0, index) + this._storage.substring(index + item.length);
+  }
 };
 
 /*
